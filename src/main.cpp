2106c47a82fe95413b5054a63b524614fff92d66
muhuchah <m.h.chah4@gmail.com>
2024-06-17 09:48:20 +0330

    check if MOD is prime

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,24 @@ int reconstructSecret(const std::vector<std::pair<int, int>>& shares, int MOD) {
     return secret;
 }
 
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    if (number <= 3) {
+        return true;
+    }
+    if (number % 2 == 0 || number % 3 == 0) {
+        return false;
+    }
+    for (int i = 5; i * i <= number; i += 6) {
+        if (number % i == 0 || number % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     int n, MOD, secret, t;
 
@@ -93,6 +111,11 @@ int main() {
     std::cin >> n;
     std::cout << "Enter the modulus (MOD): ";
     std::cin >> MOD;
+
+    if (!isPrime(MOD)) {
+        std::cerr << "MOD must be a prime number." << std::endl;
+    }
+
     std::cout << "Enter the secret: ";
     std::cin >> secret;
     std::cout << "Enter the threshold (t): ";
